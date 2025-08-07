955c147a88d386988f8a8a2760426ed51ffa05c9
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-08-07 19:38:31 +0530

    aligning the star inside the select div

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { FaRegStar } from "react-icons/fa";
+
 
 function Dropdown({ title, currencies }) {
     return (
         <div>
-            <label htmlFor={title}>{title}</label>
-            <div>
-                  <select name="" id="">
+            <label htmlFor={title} className=' block text-sm font-medium text-gray-700'>{title}</label>
+            <div className='mt-1 mb-1 relative'>
+                  <select name="" id="" className='w-full p-2 bg-gray-300 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500'>
+                    <hr />
                 {
                     currencies.map((currency) => <option key={currency} value={currencies}>{currency}</option>)
                 }
+                   
             </select>
+            <button className='absolute right-5 top-1/2 -translate-y-1/2 '><FaRegStar  /></button>
+         
+
             </div>
         </div>
     )
